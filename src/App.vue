a2733eb1dddbfe6961fdd589aa21c44ccea7cb8b
Wihtlake <wihtlake@mail.ru>
2024-06-04 17:28:21 +0500

    slider

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <carousel :items-to-show="1.5">
+    <slide v-for="slide in 1" :key="slide" slideWidth="10">
+      <img src="./img/JZA3carGFYk.jpg" width="400px" height="400px" />
+
+    </slide>
+    <slide v-for="slide in 1" :key="slide" slideWidth="10">
+      <img src="./img/RPInPR54eso.jpg" width="400px" height="400px" />
+
+    </slide>
+    <slide v-for="slide in 1" :key="slide" slideWidth="10">
+      <img src="./img/TUL5KP9tMao.jpg" width="400px" height="400px" />
+
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
+
+  <carousel :items-to-show="1.5">
+    <slide v-for="slide in 10" :key="slide">
+      {{ slide }}
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/slider.vue
@@ -0,0 +1,49 @@
+<template>
+    <carousel :items-to-show="1.5">
+      <slide v-for="slide in 1" :key="slide" slideWidth="10">
+        <img src="./img/JZA3carGFYk.jpg" width="400px" height="400px" />
+  
+      </slide>
+      <slide v-for="slide in 1" :key="slide" slideWidth="10">
+        <img src="./img/RPInPR54eso.jpg" width="400px" height="400px" />
+  
+      </slide>
+      <slide v-for="slide in 1" :key="slide" slideWidth="10">
+        <img src="./img/TUL5KP9tMao.jpg" width="400px" height="400px" />
+  
+      </slide>
+  
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+    </carousel>
+  
+    <carousel :items-to-show="1.5">
+      <slide v-for="slide in 10" :key="slide">
+        {{ slide }}
+      </slide>
+  
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+    </carousel>
+  </template>
+  
+  <script>
+  // If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+  import 'vue3-carousel/dist/carousel.css'
+  import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+  
+  export default {
+    name: 'App',
+    components: {
+      Carousel,
+      Slide,
+      Pagination,
+      Navigation,
+    },
+  }
+  </script>
+  
